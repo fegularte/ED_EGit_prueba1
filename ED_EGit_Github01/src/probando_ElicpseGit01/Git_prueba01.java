053c79aeac4bd85f360823fbccf0b4e7055ed9b3
Fede Gularte <fegulartebarboza@gmail.com>
2021-03-09 00:27:54 +0100

    probando conexion con token

--- ED_EGit_Github01/src/probando_ElicpseGit01/Git_prueba01.java
+++ ED_EGit_Github01/src/probando_ElicpseGit01/Git_prueba01.java
@@ -5,6 +5,7 @@ public class Git_prueba01 {
 		System.out.println("Prueba 1. Modificacion 1. Commit 2");
 		System.out.println("Prueba 1. Modificacion 2. Commit 3");
 		System.out.println("Prueba 1. Modificacion 2. Commit 4");
-		System.out.println("Prueba 1. Modificacion 2. Commit 4");
+		System.out.println("Prueba 1. Modificacion 2. Commit 5");
+		System.out.println("Prueba 1. Modificacion 2. Commit 6");
 	}
 }
