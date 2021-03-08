168fc0974b87bdf37fb7328af4eca20c28ee66f0
Fede Gularte <fegulartebarboza@gmail.com>
2021-03-08 17:28:11 +0100

    ED_EGit_Github01
    Prueba 1. Midificación 2. Commit 3

--- ED_EGit_Github01/src/probando_ElicpseGit01/Git_prueba01.java
+++ ED_EGit_Github01/src/probando_ElicpseGit01/Git_prueba01.java
@@ -2,6 +2,7 @@ package probando_ElicpseGit01;
 
 public class Git_prueba01 {
 	public Git_prueba01(){
-		System.out.println("Prueba 1. Modificacion 1. Commit 1");
+		System.out.println("Prueba 1. Modificacion 1. Commit 2");
+		System.out.println("Prueba 1. Modificacion 2. Commit 3");
 	}
 }
